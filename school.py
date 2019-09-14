2a674632410d1c2358ad1aead5bc1e5ab3dc616e
Zhaleh Feizollahi <zhaleh@uber.com>
2019-09-14 12:17:28 -0700

    lab complete

--- school.py
+++ school.py
@@ -1,1 +1,24 @@
-class School:
+class School():
+    def __init__(self, name=None):
+        self.name = name
+        self.roster = {}
+        pass
+
+    def add_student(self, student, grade):
+        if grade in self.roster.keys():
+            self.roster[grade].append(student)
+        else:
+            self.roster[grade] = [student]
+    
+    def grade(self, grade):
+        
+        if grade in self.roster.keys():
+            return list(self.roster[grade])
+        else:
+            return "Grade not found"
+    
+    def sort_roster(self):
+        sorted_roster = {}
+        for grade in self.roster.keys():
+            sorted_roster[grade] = sorted(list(self.roster[grade]))
+        return sorted_roster
